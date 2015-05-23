d8be711b0391783714bccab251e42758f32f86a9
Alex Morega <alex@grep.ro>
2015-05-23 16:16:47 +0300

    move drag-and-drop scxml to separate file

--- e/drag-and-drop/app.js
+++ e/drag-and-drop/app.js
@@ -1,39 +1,38 @@
-var rect = document.getElementById("rect");
+$.get('drag-and-drop.scxml', function(scxml) {
+  var rect = document.getElementById("rect");
+  var scxmlDoc = (new DOMParser()).parseFromString(scxml, "application/xml");
 
-var scxmlDocString = document.getElementById('scxmlDoc').textContent;
-var scxmlDoc = (new DOMParser()).parseFromString(scxmlDocString, "application/xml");
-
-var layout = new forceLayout.Layout({
-  parent: document.getElementById('viz'),
-  doc: scxmlDoc
-});
-layout.fit();
-
-
-scion.documentStringToModel(scxmlDocString, function(err, model) {
-
-  if(err) throw err;
-
-  //instantiate the interpreter
-  var interpreter = new scion.SCXML(model);
-
-  interpreter.registerListener({
-    onEntry: function(stateId) { layout.highlightState(stateId, true); },
-    onExit: function(stateId) { layout.highlightState(stateId, false); },
+  var layout = new forceLayout.Layout({
+    parent: document.getElementById('viz'),
+    doc: scxmlDoc
   });
+  layout.fit();
 
-  //start the interpreter
-  interpreter.start();
+  scion.documentStringToModel(scxml, function(err, model) {
 
-  //send the init event
-  interpreter.gen({name: "init", data: rect});
+    if(err) throw err;
 
-  function handleEvent(e) {
-    e.preventDefault();
-    interpreter.gen({name: e.type, data: e});
-  }
+    //instantiate the interpreter
+    var interpreter = new scion.SCXML(model);
 
-  //connect all relevant event listeners
-  $(rect).mousedown(handleEvent);
-  $(document.documentElement).bind("mouseup mousemove", handleEvent);
+    interpreter.registerListener({
+      onEntry: function(stateId) { layout.highlightState(stateId, true); },
+      onExit: function(stateId) { layout.highlightState(stateId, false); },
+    });
+
+    //start the interpreter
+    interpreter.start();
+
+    //send the init event
+    interpreter.gen({name: "init", data: rect});
+
+    function handleEvent(e) {
+      e.preventDefault();
+      interpreter.gen({name: e.type, data: e});
+    }
+
+    //connect all relevant event listeners
+    $(rect).mousedown(handleEvent);
+    $(document.documentElement).bind("mouseup mousemove", handleEvent);
+  });
 });
