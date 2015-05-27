d0c84537730fed1b0689310169e5df6fe906ecda
Alex Morega <alex@grep.ro>
2015-05-27 14:03:19 +0300

    show current state on the button

--- e/morse-code-parser/app.jsx
+++ e/morse-code-parser/app.jsx
@@ -1,3 +1,31 @@
+'use strict';
+
+class StateView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    if(this.state.initial) {
+      return <span>-</span>;
+    }
+    if(this.state.sending_dot) {
+      return <span>dot</span>;
+    }
+    if(this.state.sending_dash) {
+      return <span>dash</span>;
+    }
+    if(this.state.short_pause) {
+      return <span>short pause</span>;
+    }
+    if(this.state.long_pause) {
+      return <span>long pause</span>;
+    }
+    return <span>??</span>;
+  }
+}
+
+
 $.get('morse.scxml', function(scxml) {
   var rect = document.getElementById("rect");
   var scxmlDoc = (new DOMParser()).parseFromString(scxml, "application/xml");
@@ -14,9 +42,21 @@ $.get('morse.scxml', function(scxml) {
 
     var interpreter = new scion.SCXML(model);
 
+    var stateView = React.render(<StateView />, document.getElementById('button'));
+
     interpreter.registerListener({
-      onEntry: function(stateId) { layout.highlightState(stateId, true); },
-      onExit: function(stateId) { layout.highlightState(stateId, false); },
+      onEntry: function(stateId) {
+        var newState = {};
+        newState[stateId] = true;
+        stateView.setState(newState);
+        layout.highlightState(stateId, true);
+      },
+      onExit: function(stateId) {
+        var newState = {};
+        newState[stateId] = false;
+        stateView.setState(newState);
+        layout.highlightState(stateId, false);
+      },
     });
 
     interpreter.start();
